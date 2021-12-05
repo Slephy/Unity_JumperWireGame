70b32a78856d38bc559c77f13e5fceab024370de
Slephy <huna74nashi@icloud.com>
2021-12-05 12:17:27 +0900

    Update input.ino

--- input/input.ino
+++ input/input.ino
@@ -1,34 +1,29 @@
+// #include <Arduino.h>
+// 通信量を削減
+// 接続:1, 非接続:0としてバイナリデータで表現
+// 9通りの接続状態を9bitで通信。通信量は1/18に。
 void setup() {
     // put your setup code here, to run once:
     for(int i = 0; i < 3; i++) pinMode(11+i, OUTPUT);
-    // for(int i = 0; i < 3; i++) pinMode(2+i, INPUT);
     for(int i = 0; i < 3; i++) digitalWrite(11+i, LOW);
     Serial.begin(19200);
 }
 
 void loop() {
+    // put your main code here, to run repeatedly:
+    int send = 0;
     for(int i = 0; i < 3; i++){
         digitalWrite(13-i, HIGH);
         delay(5);
         for(int j = 0; j < 3; j++){
             int r = digitalRead(4-j);
-            // int r = analogRead(4-j);
-            // Serial.print(r);
-            // Serial.print(", ");
-
-            Serial.print(i);
-            Serial.print("\t");
-            Serial.print(j);
-            Serial.print("\t");
-            if(r == HIGH) Serial.print(1);
-            else Serial.print(0);
-            Serial.println();
+            int index = 3*i + j;
+            send += r * (1 << index);
         }
-        // Serial.println();
         digitalWrite(13-i, LOW);
-        // delay(10);
     }
-    // Serial.println();
+    Serial.print(send);
+    Serial.println();
     
     delay(10);
 }
