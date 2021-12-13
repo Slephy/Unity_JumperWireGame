0d64f19803ffb04914c5b69b8d081520bbe61572
Slephy <huna74nashi@icloud.com>
2021-12-14 00:41:01 +0900

    arrange scripts

---
+++ JumperWireGame/Assets/Scripts/Dual_Port/test_cube_serial.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_cube_serial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
