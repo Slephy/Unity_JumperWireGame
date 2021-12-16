6078ccf518559a0316de001866eeba28887e785e
Slephy <huna74nashi@icloud.com>
2021-12-17 02:22:06 +0900

    enable change ball and capsule

--- JumperWireGame/Assets/Scripts/Main_Scene/test_create_ball.cs
+++ JumperWireGame/Assets/Scripts/Main_Scene/test_create_ball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class test_create_ball : MonoBehaviour
 {
     public GameObject ball_prefab;
+    public GameObject capsule_prefab;
     public Material blue;
     public Material green;
     public Material red;
@@ -17,25 +18,36 @@ public class test_create_ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            CreateBall("red");
-        }
+        // if(Input.GetKeyDown(KeyCode.Space)){
+        //     CreateBallOrCapsule("red");
+        // }
+        if(Input.GetKeyDown(KeyCode.Alpha1)) CreateSomething("blue");
+        if(Input.GetKeyDown(KeyCode.Alpha2)) CreateSomething("red");
+        if(Input.GetKeyDown(KeyCode.Alpha3)) CreateSomething("green");
     }
 
-    private void CreateBall(string color, bool isCupsule = false){
+    private void CreateSomething(string color){
+        if(Input.GetKey(KeyCode.C)) CreateBallOrCapsule(color, true);
+        else CreateBallOrCapsule(color);
+    }
+
+    private void CreateBallOrCapsule(string color, bool isCapsule = false){
         Debug.Log("space key pressed");
-        GameObject ball = Instantiate(ball_prefab) as GameObject;
-        ball.transform.position = new Vector3(-16.5f, 2.7f, Random.Range(-14.0f, -2.0f));
+        GameObject inst;
+        if(isCapsule) inst = Instantiate(capsule_prefab) as GameObject;
+        else inst = Instantiate(ball_prefab) as GameObject;
+        // GameObject inst = Instantiate(ball_prefab) as GameObject;
+        inst.transform.position = new Vector3(-16.5f, 2.7f, Random.Range(-14.0f, -2.0f));
         switch (color)
         {
             case "blue":
-                ball.GetComponent<Renderer>().material = blue;
+                inst.GetComponent<Renderer>().material = blue;
                 break;
             case "green":
-                ball.GetComponent<Renderer>().material = green;
+                inst.GetComponent<Renderer>().material = green;
                 break;
             case "red":
-                ball.GetComponent<Renderer>().material = red;
+                inst.GetComponent<Renderer>().material = red;
                 break;
         }
     }
