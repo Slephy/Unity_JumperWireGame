7f82bc780eef15c544b0283a52e0d9f37a7ef296
Slephy <huna74nashi@icloud.com>
2021-12-17 02:07:34 +0900

    able to create ball of 3colors

--- JumperWireGame/Assets/Scripts/Main_Scene/test_create_ball.cs
+++ JumperWireGame/Assets/Scripts/Main_Scene/test_create_ball.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class test_create_ball : MonoBehaviour
 {
     public GameObject ball_prefab;
+    public Material blue;
+    public Material green;
+    public Material red;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +18,25 @@ public class test_create_ball : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space)){
-            Debug.Log("space key pressed");
-            GameObject ball = Instantiate(ball_prefab) as GameObject;
-            // ball.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
-            ball.transform.position = new Vector3(-16.5f, 2.7f, Random.Range(-14.0f, -2.0f));
+            CreateBall("red");
+        }
+    }
+
+    private void CreateBall(string color, bool isCupsule = false){
+        Debug.Log("space key pressed");
+        GameObject ball = Instantiate(ball_prefab) as GameObject;
+        ball.transform.position = new Vector3(-16.5f, 2.7f, Random.Range(-14.0f, -2.0f));
+        switch (color)
+        {
+            case "blue":
+                ball.GetComponent<Renderer>().material = blue;
+                break;
+            case "green":
+                ball.GetComponent<Renderer>().material = green;
+                break;
+            case "red":
+                ball.GetComponent<Renderer>().material = red;
+                break;
         }
     }
 
