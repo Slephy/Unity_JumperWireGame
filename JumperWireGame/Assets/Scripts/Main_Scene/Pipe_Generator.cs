d2266f57db2fffc4735d139b7ed953b7134a35a2
Slephy <huna74nashi@icloud.com>
2021-12-31 11:49:22 +0900

    Update Pipe_Generator.cs and so on
    Arduinoからのシリアル通信に応じて、パイプの表示非表示を切り替えられるようにしました。

--- JumperWireGame/Assets/Scripts/Main_Scene/Pipe_Generator.cs
+++ JumperWireGame/Assets/Scripts/Main_Scene/Pipe_Generator.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Pipe_Generator : MonoBehaviour
 {
+    [SerializeField] private Serial_Handler serialHandler;
+    [SerializeField] private Test_Manager testManager;
+    private bool isTest;
     private GameObject[,,] pipeParts = new GameObject[3, 3, 4];
     private string[,] pipeName = {{"pipe_1(Blue)", "pipe_2(Green-1)", "pipe_3(Red)"},
                                   {"pipe_2(Blue)", "pipe_1(Green)",   "pipe_2(Red)"},
                                   {"pipe_3(Blue)", "pipe_2(Green-2)", "pipe_1(Red)"}};
+
     // Start is called before the first frame update
     void Start()
     {
+        // serialHandlerの初期化
+        serialHandler.OnDataReceived += OnDataReceived;
+
+        // isTestの初期化
+        isTest = testManager.CheckIfTest();
+
+        // pipePartsの初期化
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
                 Transform pipe = GameObject.Find(pipeName[i, j]).GetComponent<Transform>();
@@ -31,10 +43,12 @@ public class Pipe_Generator : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                if(Input.GetKeyDown((KeyCode)(KeyCode.Keypad1 + (3*i + j)))){
-                    CreatePipe(i, j);
+        if(isTest){
+            for (int i = 0; i < 3; i++){
+                for (int j = 0; j < 3; j++){
+                    if(Input.GetKeyDown((KeyCode)(KeyCode.Keypad1 + (3*i + j)))){
+                        CreatePipe(i, j);
+                    }
                 }
             }
         }
@@ -48,5 +62,18 @@ public class Pipe_Generator : MonoBehaviour
 
     void ChangeActiveState(GameObject g){
         g.SetActive(!g.activeSelf);
+    }
+
+    void OnDataReceived(string message){
+        int data = Int32.Parse(message);
+        Debug.Log(message);
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                bool isActive = ((data & (1 << (3*i + j))) > 0);
+                for(int k = 0; k < 4; k++){
+                    pipeParts[i, j, k].SetActive(isActive);
+                }
+            }
+        }
     }
 }
