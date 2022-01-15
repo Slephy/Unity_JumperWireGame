83ae269fa5b86d7d877b4a65b814b901b68116e9
Slephy <huna74nashi@icloud.com>
2022-01-16 06:06:05 +0900

    Update Local_Gravity_Manager.cs and so on

--- JumperWireGame/Assets/Scripts/Main_Scene/Local_Gravity_Manager.cs
+++ JumperWireGame/Assets/Scripts/Main_Scene/Local_Gravity_Manager.cs
@@ -7,21 +7,16 @@ public class Local_Gravity_Manager : MonoBehaviour
     private Vector3 localGravity;
     private Rigidbody rBody;
     private const float SECONDPIPE_X = 4.0f;
-    private const float DECELERATE_X = 10.0f;
-    private const float END_X = 12.0f;
+    // private const float DECELERATE_X = 10.0f;
+    // private const float END_X = 12.0f;
 
-    [SerializeField] private Vector3[] GRAVITY = new[] {new Vector3(0.0f, -7.0f, 0.0f),
-                                                        new Vector3(10.0f, -30.0f, 0.0f),
-                                                        new Vector3(5.0f, -5.0f, 0.0f),
-                                                        new Vector3(0.0f, -5.0f, 0.0f),
-    };
-    [SerializeField] private Vector3[] VELOCITY = new[] {new Vector3(0.0f, 0.0f, 0.0f),
-                                                         new Vector3(5.0f, -10.0f, 0.0f),
-                                                         new Vector3(0.0f, 0.0f, 0.0f),
-                                                         new Vector3(0.5f, 0.0f, 0.0f),
+    private Vector3[] GRAVITY = new[] {new Vector3(0.0f, -7.0f, 0.0f),
+                                       new Vector3(10.0f, -10.0f, 0.0f),
+                                       new Vector3(5.0f, -5.0f, 0.0f),
+                                       new Vector3(0.0f, -5.0f, 0.0f),
     };
 
-    private enum BallState{
+    enum BallState{
         onStage,
         inPipeFirst,
         inPipeSecond,
@@ -30,7 +25,6 @@ public class Local_Gravity_Manager : MonoBehaviour
 
     [SerializeField] private BallState state;
 
-    private bool isXDecelerated = false;
 
     // Use this for initialization
     private void Start() {
@@ -50,42 +44,12 @@ public class Local_Gravity_Manager : MonoBehaviour
                     ChangeGravityAndVelocity(state);
                 }
                 break;
-
-            case BallState.inPipeSecond:
-                if(x >= DECELERATE_X){
-                    state = BallState.Decelerating;
-                    ChangeGravityAndVelocity(state);
-                }
-                break;
-
         }
-
-        // if(state == BallState.Decelerating && rBody.velocity.x <= 1.0f){
-        //     state = BallState.End;
-        //     ChangeGravityAndVelocity(state);
-        // }
-    }
-
-
-    public void ChangeStateTo_InPipeFirst(){
-        state = BallState.inPipeFirst;
-        ChangeGravityAndVelocity(state);
-        Debug.Log("state is changed to inPipeFirst");
-    }
-
-
-    public void ChangeStateTo_End(){
-        state = BallState.End;
-        ChangeGravityAndVelocity(state);
-        Debug.Log("state is changed to End");
     }
 
 
     void ChangeGravityAndVelocity(BallState state){
         localGravity = GRAVITY[(int)state];
-        if(state == BallState.inPipeFirst || state == BallState.End){
-            rBody.velocity = VELOCITY[(int)state];
-        }
     }
 
 
@@ -97,19 +61,31 @@ public class Local_Gravity_Manager : MonoBehaviour
         rBody.AddForce (localGravity, ForceMode.Acceleration);
     }
 
-    private void ChangeLocalGravity_oneDimention(int dim, float gravity){
-        switch(dim){
-            case 0:
-                localGravity.x = gravity;
-                break;
 
-            case 1:
-                localGravity.y = gravity;
-                break;
+    public void ChangeStateTo_InPipeFirst(){
+        state = BallState.inPipeFirst;
+        ChangeGravityAndVelocity(state);
+        StartCoroutine(pushBall());
+        Debug.Log("state is changed to inPipeFirst");
+    }
 
-            case 2:
-                localGravity.z = gravity;
-                break;
-        }
+
+    public void ChangeStateTo_End(){
+        state = BallState.End;
+        bind_XZpos();
+        ChangeGravityAndVelocity(state);
+        Debug.Log("state is changed to End");
+    }
+
+
+    void bind_XZpos(){
+        rBody.constraints = RigidbodyConstraints.FreezePositionX;
+        rBody.constraints = RigidbodyConstraints.FreezePositionZ;
+    }
+
+    IEnumerator pushBall(){
+        rBody.AddForce(new Vector3(7, 0, 0), ForceMode.Impulse);
+        yield return new WaitForSeconds(0.2f);
+        rBody.AddForce(new Vector3(0, -7, 0), ForceMode.Impulse);
     }
 }
