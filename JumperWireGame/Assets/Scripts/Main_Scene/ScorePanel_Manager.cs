61ea075c2a653bcb4f3f2b66520fb8561d277798
Slephy <huna74nashi@icloud.com>
2022-01-25 11:01:08 +0900

    Update ScorePanel

--- JumperWireGame/Assets/Scripts/Main_Scene/ScorePanel_Manager.cs
+++ JumperWireGame/Assets/Scripts/Main_Scene/ScorePanel_Manager.cs
@@ -12,7 +12,7 @@ public class ScorePanel_Manager : MonoBehaviour
     [SerializeField] private Button btnHome;
     [SerializeField] private TextMeshProUGUI yourScore;
     [SerializeField] private TextMeshProUGUI _score;
-    [SerializeField] private TextMeshProUGUI perfect;
+    [SerializeField] private TextMeshProUGUI message;
 
     void Start(){
         scorePanel.gameObject.SetActive(false);
@@ -20,7 +20,7 @@ public class ScorePanel_Manager : MonoBehaviour
         btnHome.gameObject.SetActive(false);
         yourScore.gameObject.SetActive(false);
         _score.gameObject.SetActive(false);
-        perfect.gameObject.SetActive(false);
+        message.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -42,24 +42,32 @@ public class ScorePanel_Manager : MonoBehaviour
         _score.text = score.ToString();
         _score.gameObject.SetActive(true);
 
-        float getPercent = score / ballQuantity; 
+        float getPercent = 1.0f * score / ballQuantity; 
+        Debug.Log("Your getPercent = " + score);
+        Debug.Log("Your ballQuantity = " + ballQuantity);
+        Debug.Log("Your getPercent = " + getPercent);
         // Perfect
         if(score == ballQuantity){
             sePlayer.Play(SE_Manager.kind.result_perfect);
-            perfect.gameObject.SetActive(true);
+            message.text = "Perfect!!";
         }
         // Great
-        else if(getPercent >= 0.8f){
+        else if(getPercent >= 0.7f){
             sePlayer.Play(SE_Manager.kind.result_great);
+            message.text = "Great!";
         }
         // Good
-        else if(getPercent >= 0.5f){
+        else if(getPercent >= 0.3f){
             sePlayer.Play(SE_Manager.kind.result_good);
-            
+            message.text = "Good!";
         }
         // Bad
-        else sePlayer.Play(SE_Manager.kind.result_bad);
-        // else sePlayer.Play(SE_Manager.kind.result_dodon);
+        else{
+            sePlayer.Play(SE_Manager.kind.result_bad);
+            message.text = "Try Again!";
+        }
+        message.gameObject.SetActive(true);
+
         yield return new WaitForSeconds(0.5f);
         
         btnRetry.gameObject.SetActive(true);
